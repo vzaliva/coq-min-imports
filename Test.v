894ee726a93da4c54fb6797005e03aac595ba1cc
Vadim Zaliva <lord@crocodile.org>
2016-07-24 13:49:47 -0700

    testing example

---
+++ Test.v
@@ -0,0 +1,16 @@
+Require Import Bool.DecBool.
+Require Import Arith.Min.
+Require Import Omega.
+Require Import BinNums List Setoid.
+
+(* requires Arith.Min *)
+Definition bar1 := min_0_l.
+
+(* requires Omega *)
+Lemma foo: 1<2.
+Proof.
+  omega.
+Qed.
+
+(* Requires List *)
+Definition bar2 := hd.

513a9182a8318c7e210b0ed73a162d1b59255aa0
Vadim Zaliva <lord@crocodile.org>
2016-08-22 21:17:31 -0700

    processing single import statement

--- coq_min_imports.ml
+++ coq_min_imports.ml
@@ -1,6 +1,6 @@
 open Std
 open Str
-open BatArray
+open BatList
        
 (* TODO: match mutliline import statements *)
 let import_regexp = Str.regexp "^[ \t]*Require[ \t]+Import[ \t]+\\(.+\\)\\.[\t ]*"
@@ -8,19 +8,42 @@ let import_regexp = Str.regexp "^[ \t]*Require[ \t]+Import[ \t]+\\(.+\\)\\.[\t ]
 let verbose = ref false
                                
 let parse_cmd_line () =
+  let open BatArray in
   verbose := exists (String.equal "-cmi-verbose") Sys.argv;
   let fname_regexp = regexp "[A-Za-z_][A-Za-z_']+\\.v" in (* TODO: unicode *)
   let newargs = filter (fun x -> not (BatString.starts_with x "-cmi-") && not (string_match fname_regexp x 0)) Sys.argv in 
   (newargs, filter (fun x -> string_match fname_regexp x 0) Sys.argv)
 
+let try_compile s =
+  false
+
+let rec process_require pre post lst res =
+  match lst with
+  | [] -> res
+  | x::xs ->
+     Printf.printf "*** Trying to remove %s\n" x;
+     let nl = ((rev xs)@res) in
+     let body = pre ^
+                  (if is_empty nl then "" else
+                     "Require Import " ^ (String.concat " " nl) ^ ".\n")
+                  ^ post in
+     Printf.printf "\n==================\n %s \n==================\n" body;
+     process_require pre post xs
+                     (if try_compile body then res else (cons x res))
+  
 let rec process_imports s p =
   try
     let x = search_backward import_regexp s p in
     let is = (matched_group 1 s) in
-    let isb = group_beginning 1 in
-    let ise = group_end 1 in
+    let me = match_end () in
     let il = Str.split (regexp "[ \t]+") is in
     Printf.printf "\t%d: %s (%d)\n" x is (List.length il);
+    let _ = process_require
+              (string_before s x)
+              (string_after s me)
+              (rev il)
+              []
+    in
     if x=0 then s
     else process_imports s (x-1)
   with
@@ -33,6 +56,6 @@ let process_file fname =
                                
 let () =
   let (args,files) = parse_cmd_line () in
-  ignore (map process_file files)
+  ignore (BatArray.map process_file files)
   
                 

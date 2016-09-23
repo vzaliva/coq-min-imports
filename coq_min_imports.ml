15e6d61acad9cd22cff735a38f2e78dc9ac6b72e
Vadim Zaliva <lord@crocodile.org>
2016-09-22 17:50:36 -0700

    usage

--- coq_min_imports.ml
+++ coq_min_imports.ml
@@ -27,14 +27,14 @@ let parse_cmd_line () =
 
 let try_compile s =
   let d = make_tmp_dir 0o755 ~prefix:"coq_min_imports" ~suffix:".tmpdir" in
-  let (out, name) = open_temporary_out ~mode:[`delete_on_exit ; `create] ~suffix:".v" ~temp_dir:d () in
+  let (out, name) = open_temporary_out ~mode:[`create] ~suffix:".v" ~temp_dir:d () in
   write_line out s;
   close_out out;
   (* TODO: use fork/execve to make sure arguments are properly passed *)
   let cmd = (!coqcmd) ^ " " ^ (String.concat " " !coqargs) ^ " " ^ name ^ " > /dev/null 2>&1" in
   if !debug then Printf.printf "Executing: %s\n" cmd;
   let res = BatSys.command cmd in
-  rmrf d ; (*TODO: make sure directory removed on exceptin *)
+  rmrf d ;
   (res == 0)
 
 let gen_import lst =
@@ -95,5 +95,7 @@ let process_file fname =
 
 let () =
   let (args,files) = parse_cmd_line () in
-  coqargs := tl (Array.to_list args);
-  ignore (BatArray.map process_file files)
+  if Array.length files = 0 then
+    (Printf.printf "Usage: coq_min_imports <coq_flags> <files...>\n" ; exit 1)
+  else
+    (coqargs := tl (Array.to_list args); ignore (BatArray.map process_file files))
